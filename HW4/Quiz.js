9bb9359f9f442342fef800331d0f87c136a944fa
Brad Bryant <bradleybryant86@gmail.com>
2019-12-16 23:18:02 -0600

    Wrote quiz questions and formatted them with answer choices

---
+++ HW4/Quiz.js
@@ -0,0 +1,118 @@
+var questions = [
+    {
+        question: "The Mandalorian's armor is made of what material?",
+        choices: {
+            a: "Steel",
+            b: "Titanium",
+            c: "Cobalt",
+            d: "Beskar"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Would you give your life for Baby Yoda?",
+        choices: {
+            a: "Absolutely",
+            b: "Nope",
+            c: "I wanna live",
+            d: "Nah"
+        },
+        correctAnswer: "Absolutely"
+    },
+    {
+        question: "What weapon does the Mandalorian have on his left wrist?",
+        choices: {
+            a: "Knife",
+            b: "Flamethrower",
+            c: "Taser",
+            d: "A blaster"
+        },
+        correctAnswer: "Flamethrower"
+    },
+    {
+        question: "What is the Mandalorian's moral code?",
+        choices: {
+            a: "Hooyah",
+            b: "Rangers Lead the Way",
+            c: "Get some",
+            d: "This is the way"
+        },
+        correctAnswer: "This is the way"
+    },
+    {
+        question: "Who does Mando hunt down in order to get his stolen spaceship parts back from?",
+        choices:  {
+            a: "Jawas",
+            b: "Tusken Raiders",
+            c: "Stormtroopers",
+            d: "Jedi"
+        },
+        correctAnswer: "Jawas"
+    },
+    {
+        question: "How many years post-Empire does the Mandalorian take place?",
+        choices:  {
+            a: "28",
+            b: "11",
+            c: "20",
+            d: "5"
+        },
+        correctAnswer: "5"
+    },
+    {
+        question: "What are the Mandalorians known for?",
+        choices:  {
+            a: "Pod-racing",
+            b: "Smuggling",
+            c: "Bounty hunting",
+            d: "Blue milk"
+        },
+        correctAnswer: "Bounty hunting"
+    },
+    {
+        question: "What group is Mando a part of?",
+        choices:  {
+            a: "The First Order",
+            b: "The Guild",
+            c: "The Mos Eisley Cantina Band",
+            d: "Jabba The Hutt's Gang"
+        },
+        correctAnswer: "The Guild"
+    },
+    {
+        question: "Who does the Empire want Mando to find for them?",
+        choices:  {
+            a: "Kylo Ren",
+            b: "Greedo",
+            c: "Baby Yoda",
+            d: "Wicket the Ewok"
+        },
+        correctAnswer: "Baby Yoda"
+    },
+    {
+        question: "What character threatens to initiate self-destruct in episode 1?",
+        choices:  {
+            a: "IG-11, the assasin droid",
+            b: "Yub Nub",
+            c: "Bossk",
+            d: "Boba Fett"
+        },
+        correctAnswer: "IG-11"
+    },
+    {
+        question: "What can't a Mandalorian do?",
+        choices:  {
+            a: "Work with others",
+            b: "Kill enemies",
+            c: "Remove his helmet",
+            d: "Shoot first"
+        },
+        correctAnswer: "Remove his helmet"
+    },
+]
+
+var score = 0;
+
+for (var i = 0; i < questions.length; i++) {
+    var response = w
+}
